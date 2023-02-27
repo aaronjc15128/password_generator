501171ae8d9e8ed3cc04ddf8456dd0d5f20aa900
Aaron Chauhan <100516190+aaronjc15128@users.noreply.github.com>
2023-02-27 20:56:44 +0000

    Added stateless widget for the main app

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,29 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const App(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
   
   int navBarIndex = 0;
 
@@ -80,10 +95,6 @@ class _MyAppState extends State<MyApp> {
             BottomNavigationBarItem(
               icon: Icon(Icons.check_circle_outline_rounded),
               label: "Strength Checker",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.question_mark_rounded),
-              label: "???",
             ),
           ],
           currentIndex: navBarIndex,
